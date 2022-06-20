823cf6d946f7f1d8b9156b8d27d6097ee0f4f1af
Rufliex <danilkl255@mail.ru>
2022-06-20 17:19:12 +0300

    added slider optoions

--- AboutMySelf-main/myPagePhis/slider.js
+++ AboutMySelf-main/myPagePhis/slider.js
@@ -4,15 +4,15 @@ const sliderLine= document.querySelector('.sliderphotoes')
 
 document.querySelector('.slider-next').addEventListener('click',function (){
     offset = offset + 123;
-    if (offset > 493){
+    if (offset > 369){
         offset = 0
     }
     sliderLine.style.left = -offset + 'px'
 })
 document.querySelector('.slider-previous').addEventListener('click',function (){
     offset = offset -123;
-    if (offset < -493 ){
-        offset = 0
+    if (offset < 0 ){
+        offset = 369
     }
     sliderLine.style.left = -offset + 'px'
 })
