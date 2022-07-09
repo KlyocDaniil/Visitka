1762a9bb3180990b8249d01ec32d325f1d3b593d
Rufliex <danilkl255@mail.ru>
2022-07-09 14:00:03 +0300

    finish pop up
    добавил пространство для изменения страницы( поп ап окно)

--- AboutMySelf-main/myPagePhis/scripts.js
+++ AboutMySelf-main/myPagePhis/scripts.js
@@ -128,3 +128,16 @@ const deletePost = index =>{
 document.getElementById("checkNewsForm").onsubmit = function (){
     return false
 }
+
+const openPopUp = document.getElementById("open_pop_up")
+const closePopUp = document.getElementById("pop_up_close")
+const popUp = document.getElementById("pop_up")
+
+openPopUp.addEventListener('click',function (e){
+    e.preventDefault(); // уберем ненужное поведение кнопки
+    popUp.classList.add('active');
+})
+
+closePopUp.addEventListener('click', () =>{
+    popUp.classList.remove('active')
+})
